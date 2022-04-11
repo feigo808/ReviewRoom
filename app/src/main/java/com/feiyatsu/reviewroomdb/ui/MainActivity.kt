07f71c1b3eb6575b3780b6c2388dfb15f822b591
feigo808 <feigo.code@gmail.com>
2022-04-11 11:40:13 -1000

    Set up navigation

---
+++ app/src/main/java/com/feiyatsu/reviewroomdb/ui/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.feiyatsu.reviewroomdb.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.feiyatsu.reviewroomdb.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = binding.fragmentContainerView.getFragment<NavHostFragment>().navController
+
+        setupActionBarWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/feiyatsu/reviewroomdb/ui/list/ListFragment.kt
@@ -0,0 +1,36 @@
+package com.feiyatsu.reviewroomdb.ui.list
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.feiyatsu.reviewroomdb.R
+import com.feiyatsu.reviewroomdb.databinding.FragmentListBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ListFragment : Fragment() {
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+
+        binding.floatingActionButton.setOnClickListener{
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
